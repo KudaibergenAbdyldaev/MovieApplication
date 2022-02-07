364c347880e98afbbbff0f151dd52513d5e39657
Kudaibergen Abdyldaev <fsDjq!cWK:5LeD->
2022-02-07 21:56:22 +0600

    first commit

---
+++ app/src/main/java/com/adnroid/movieapplication/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adnroid.movieapplication.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adnroid.movieapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
