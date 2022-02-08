c0f58ef6b237321ff832cd0416b12b5e163d2ac9
Kudaibergen Abdyldaev <fsDjq!cWK:5LeD->
2022-02-08 19:54:24 +0600

    create domain layer

---
+++ app/src/main/java/com/adnroid/movieapplication/domain/Movie.kt
@@ -0,0 +1,7 @@
+package com.adnroid.movieapplication.domain
+
+
+data class Movie(
+    val page: Int,
+    val results: Results
+)
---
+++ app/src/main/java/com/adnroid/movieapplication/domain/Results.kt
@@ -0,0 +1,8 @@
+package com.adnroid.movieapplication.domain
+
+data class Results(
+    val img: String? = null,
+    val id: Int? = null,
+    val title: String? = null,
+    val overview: String
+)
