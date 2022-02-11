9bf60cbf40623d792c09cf1f8e5f5b6dcf3cb0d1
@kudaibergen_abdyldaev <abdyldaevk02@gmail.com>
2022-02-11 16:47:17 +0600

    create useCase and map MovieMapper

--- app/src/main/java/com/adnroid/movieapplication/data/mapper/MovieMapper.kt
+++ app/src/main/java/com/adnroid/movieapplication/data/mapper/MovieMapper.kt
@@ -1,4 +1,19 @@
 package com.adnroid.movieapplication.data.mapper
 
+import com.adnroid.movieapplication.data.network.model.MovieDto
+import com.adnroid.movieapplication.data.network.model.ResultsDto
+import com.adnroid.movieapplication.domain.Movie
+import com.adnroid.movieapplication.domain.Results
+
 class MovieMapper {
+
+    fun mapDtoToModel(dto: MovieDto) = Movie(
+        page = dto.page,
+        results = mapResultsDtoToResults(dto.results)
+    )
+
+    private fun mapResultsDtoToResults(results: ResultsDto): Results{
+        return Results(results.img, results.id, results.title, results.overview)
+    }
+
 }
